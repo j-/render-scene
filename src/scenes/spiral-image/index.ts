fabcd58b15fa677c9344dcbdffb9769af0411ae2
j- <j@skeoh.com>
2021-02-13 17:17:20 +1100

    Move line drawing into scene method

--- src/scenes/spiral-image/index.ts
+++ src/scenes/spiral-image/index.ts
@@ -21,77 +21,6 @@ const lineIntersection = (m1: number, b1: number, m2: number, b2: number): Point
   return { x, y };
 }
 
-const pStr = (point: Point2D) => `${point.x},${point.y} `;
-
-const getPath = (
-  center: Point2D,
-  startRadius: number,
-  spacePerLoop: number,
-  startTheta: number,
-  endTheta: number,
-  thetaStep: number,
-): string => {
-  // Rename spiral parameters for the formula r = a + bθ
-  const a = startRadius;  // start distance from center
-  const b = spacePerLoop / PI / 2; // space between each loop
-
-  // convert angles to radians
-  let newTheta = startTheta;
-  let oldTheta = newTheta;
-
-  // radii
-  let oldR: number;
-  let newR = a + b * newTheta;
-
-  // start and end points
-  const oldPoint: Point2D = { x: NaN, y: NaN };
-  const newPoint: Point2D = {
-    x: center.x + newR * Math.cos(newTheta),
-    y: center.y + newR * Math.sin(newTheta)
-  };
-
-  // slopes of tangents
-  let oldSlope: number;
-  let newSlope = (b * Math.sin(oldTheta) + (a + b * newTheta) * Math.cos(oldTheta)) /
-                 (b * Math.cos(oldTheta) - (a + b * newTheta) * Math.sin(oldTheta));
-
-  let path = 'M ' + pStr(newPoint);
-
-  while (oldTheta < endTheta - thetaStep) {
-    oldTheta = newTheta;
-    newTheta += thetaStep;
-
-    oldR = newR;
-    newR = a + b * newTheta;
-
-    oldPoint.x = newPoint.x;
-    oldPoint.y = newPoint.y;
-    newPoint.x = center.x + newR * Math.cos(newTheta);
-    newPoint.y = center.y + newR * Math.sin(newTheta);
-
-    // Slope calculation with the formula:
-    // (b * sinΘ + (a + bΘ) * cosΘ) / (b * cosΘ - (a + bΘ) * sinΘ)
-    const aPlusBTheta = a + b * newTheta;
-
-    oldSlope = newSlope;
-    newSlope = (b * Math.sin(newTheta) + aPlusBTheta * Math.cos(newTheta)) /
-               (b * Math.cos(newTheta) - aPlusBTheta * Math.sin(newTheta));
-
-    const oldIntercept = -(oldSlope * oldR * Math.cos(oldTheta) - oldR * Math.sin(oldTheta));
-    const newIntercept = -(newSlope * newR* Math.cos(newTheta) - newR * Math.sin(newTheta));
-
-    const controlPoint = lineIntersection(oldSlope, oldIntercept, newSlope, newIntercept);
-
-    // Offset the control point by the center offset.
-    controlPoint.x += center.x;
-    controlPoint.y += center.y;
-
-    path += 'Q ' + pStr(controlPoint) + pStr(newPoint);
-  }
-
-  return path;
-}
-
 export default class extends Scene {
   private readonly ORIGIN: Point2D = { x: this.width / 2, y: this.height / 2 };
   private readonly LOOPS = 60;
@@ -107,16 +36,84 @@ export default class extends Scene {
     ctx.fillRect(0, 0, width, height);
     ctx.beginPath();
     const p = compose(easeInSin)(frame.progress);
-    const path = new Path2D(
-      getPath(
-        ORIGIN,
-        START_RADIUS,
-        SPACE_PER_LOOP,
-        START_THETA,
-        p * END_THETA,
-        STEP_THETA,
-      ),
+    this.drawPath(
+      ORIGIN,
+      START_RADIUS,
+      SPACE_PER_LOOP,
+      START_THETA,
+      p * END_THETA,
+      STEP_THETA,
     );
-    ctx.stroke(path);
+  }
+
+  drawPath (
+    center: Point2D,
+    startRadius: number,
+    spacePerLoop: number,
+    startTheta: number,
+    endTheta: number,
+    thetaStep: number,
+  ) {
+    const { ctx } = this;
+
+    // Rename spiral parameters for the formula r = a + bθ
+    const a = startRadius;  // start distance from center
+    const b = spacePerLoop / PI / 2; // space between each loop
+
+    // convert angles to radians
+    let newTheta = startTheta;
+    let oldTheta = newTheta;
+
+    // radii
+    let oldR: number;
+    let newR = a + b * newTheta;
+
+    // start and end points
+    const oldPoint: Point2D = { x: NaN, y: NaN };
+    const newPoint: Point2D = {
+      x: center.x + newR * Math.cos(newTheta),
+      y: center.y + newR * Math.sin(newTheta)
+    };
+
+    // slopes of tangents
+    let oldSlope: number;
+    let newSlope = (b * Math.sin(oldTheta) + (a + b * newTheta) * Math.cos(oldTheta)) /
+                   (b * Math.cos(oldTheta) - (a + b * newTheta) * Math.sin(oldTheta));
+
+    ctx.moveTo(newPoint.x, newPoint.y);
+
+    while (oldTheta < endTheta - thetaStep) {
+      oldTheta = newTheta;
+      newTheta += thetaStep;
+
+      oldR = newR;
+      newR = a + b * newTheta;
+
+      oldPoint.x = newPoint.x;
+      oldPoint.y = newPoint.y;
+      newPoint.x = center.x + newR * Math.cos(newTheta);
+      newPoint.y = center.y + newR * Math.sin(newTheta);
+
+      // Slope calculation with the formula:
+      // (b * sinΘ + (a + bΘ) * cosΘ) / (b * cosΘ - (a + bΘ) * sinΘ)
+      const aPlusBTheta = a + b * newTheta;
+
+      oldSlope = newSlope;
+      newSlope = (b * Math.sin(newTheta) + aPlusBTheta * Math.cos(newTheta)) /
+                  (b * Math.cos(newTheta) - aPlusBTheta * Math.sin(newTheta));
+
+      const oldIntercept = -(oldSlope * oldR * Math.cos(oldTheta) - oldR * Math.sin(oldTheta));
+      const newIntercept = -(newSlope * newR* Math.cos(newTheta) - newR * Math.sin(newTheta));
+
+      const controlPoint = lineIntersection(oldSlope, oldIntercept, newSlope, newIntercept);
+
+      // Offset the control point by the center offset.
+      controlPoint.x += center.x;
+      controlPoint.y += center.y;
+
+      ctx.bezierCurveTo(oldPoint.x, oldPoint.y, controlPoint.x, controlPoint.y, newPoint.x, newPoint.y);
+    }
+
+    ctx.stroke();
   }
 }
